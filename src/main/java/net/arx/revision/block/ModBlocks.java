81213c09a52542507f2f685f6f988a648a6e0196
Abdul Rafay <revisionmod.arx@gmail.com>
2025-09-08 13:30:08 -0700

    git remote add origin https://github.com/revisionmodarx/Fabric-ReVision-1.21.X.git git branch -M main git push -u origin main

--- src/main/java/net/arx/revision/block/ModBlocks.java
+++ src/main/java/net/arx/revision/block/ModBlocks.java
@@ -22,44 +22,67 @@ public class ModBlocks {
             )
     );
 
-
     public static final Block SPRUCE_PANELLING = registerBlock("spruce_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block BIRCH_PANELLING = registerBlock("birch_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block JUNGLE_PANELLING = registerBlock("jungle_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block ACACIA_PANELLING = registerBlock("acacia_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block DARK_OAK_PANELLING = registerBlock("dark_oak_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block MANGROVE_PANELLING = registerBlock("mangrove_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block CHERRY_PANELLING = registerBlock("cherry_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block BAMBOO_PANELLING = registerBlock("bamboo_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block PALE_OAK_PANELLING = registerBlock("pale_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).burnable().instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block CRIMSON_PANELLING = registerBlock("crimson_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).instrument(NoteBlockInstrument.BASS)
+            )
+    );
 
     public static final Block WARPED_PANELLING = registerBlock("warped_panelling",
-            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).instrument(NoteBlockInstrument.BASS)));
+            new Block(AbstractBlock.Settings.create().strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD).instrument(NoteBlockInstrument.BASS)
+            )
+    );
    //                       ----- END OF WOOD PANELLING BLOCKS -----
 
     //                              ----- RUBY BLOCKS -----
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
-            new Block(AbstractBlock.Settings.create().strength(5.0f, 6.0f).sounds(BlockSoundGroup.METAL).requiresTool().instrument(NoteBlockInstrument.HARP)));
+            new Block(AbstractBlock.Settings.create().strength(5.0f, 6.0f).sounds(BlockSoundGroup.METAL).requiresTool().instrument(NoteBlockInstrument.HARP)
+            )
+    );
 
     //                          ----- END OF RUBY BLOCKS -----
 
